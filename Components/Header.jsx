8715f7d6db4cb101a1a7085db6d9aa5a3f4331e8
debbdev <72915973+debbdev@users.noreply.github.com>
2024-03-26 14:28:02 +0100

    Initial commit

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
             <div class="col-xl-2 col-lg-2 col-md-3 d-flex align-items-center">
               <div class="logo">
                 <a href="/">
-                  <img src="img/logo/blx.png" alt="" />
+                  <img src="img/logo/blx.png" alt="" width={120} height={50} />
                 </a>
               </div>
             </div>
